4595d9421d9181a996c098a805f9ccfdc2b1de8a
VKen <vken85@gmail.com>
2020-01-14 18:35:59 +0800

    added mongoose models for unique sequence creation for short_url

--- server.js
+++ server.js
@@ -12,8 +12,45 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+/** this project needs a db !! **/
+mongoose.connect(process.env.DB, {useNewUrlParser: true, useFindAndModify: false});
+
+// for using as increment counter identifier
+const SEQUENCE_NAME = "url_sequence";
+
+// schemas
+const CounterSchema = new mongoose.Schema({
+    _id: String,
+    seq: Number,
+})
+// models
+const Counter = mongoose.model('Counter', CounterSchema);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("mongodb connected");
+    // check sequence db used for this app
+    console.log(`checking for application-needed counter sequence: ${ SEQUENCE_NAME }`);
+    const query = Counter.where({_id: SEQUENCE_NAME});
+    query.findOne((err, counter) => {
+        if (err) return console.error(`error querying for ${ SEQUENCE_NAME }!`);
+        if (counter) {
+            console.log(`found ${ SEQUENCE_NAME }! Continuing...`);
+            // no need to do anything
+        } else {
+            console.log(`${ SEQUENCE_NAME } not found. Creating...`);
+            let url_seq = new Counter({
+                _id: SEQUENCE_NAME,
+                seq: 0
+            })
+            url_seq.save((err, url_seq) => {
+                if (err) return console.error(`error creating ${ SEQUENCE_NAME }!`);
+                console.log(`created ${ SEQUENCE_NAME } successfully`)
+            });
+        }
+    });
+
+});
 
 app.use(cors());
 
@@ -40,6 +77,19 @@ app.get("/api/hello", function (req, res) {
 app.post("/api/shorturl/new", async function (req, res) {
     const target_url = req.body.url;
 });
+
+async function getNextSequence(name) {
+    let counter = await Counter.findByIdAndUpdate(
+        name,
+        {
+            $inc: { seq: 1 }
+        },
+        {
+            new: true,
+        },
+    )
+    return counter.seq;
+}
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
