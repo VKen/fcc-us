3673417a5feff0e7b43f50f69cd7cccbec2a8c12
VKen <vken85@gmail.com>
2020-01-14 18:34:18 +0800

    loaded bodyParser to parse JSON POST

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 var cors = require('cors');
 
@@ -18,6 +19,10 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+// express 4.16.0 includes their own parser  express.json and express.urlencoded
+// https://github.com/expressjs/express/blob/master/History.md#4160--2017-09-28
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,6 +36,10 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// create new shorted url
+app.post("/api/shorturl/new", async function (req, res) {
+    const target_url = req.body.url;
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
